03ea3ab666f70b17416d453e8f2d223016ef987d
pedrophenrique <pedrohenriquegomes-2000@hotmail.com>
2023-03-23 22:26:58 -0300

    Add arquivo 'alterar-add'

---
+++ aula1/alterar-add.js
@@ -0,0 +1,16 @@
+const pessoa = {
+    nome: "Pedro",
+    idade: 23,
+    email: "email@email.com",
+};
+
+console.log(pessoa.nome);
+console.log(pessoa.tel);
+
+pessoa.tel = "31999999999";
+
+console.log(pessoa.tel);
+
+pessoa.nome = "Pedro Henrique";
+
+console.log(pessoa.nome);
